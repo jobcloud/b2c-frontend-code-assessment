5807f6e40c60f3e5116090db2eebc2ec27c8bcf3
Ariane Hy <ariane.hy@jobcloud.ch>
2021-06-11 11:04:44 +0200

    add task 1

---
+++ task_1/Posts.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Posts() {
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
---
+++ task_1/_Task.md
@@ -0,0 +1,4 @@
+# Task 1: Refactoring, Code Review
+
+1. Have a look at the Posts.js:
+   We would like to reuse this request for data across multiple component, how would you refactore it?
